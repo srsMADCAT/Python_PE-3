6f4cacf87f0217256fb24df8344233ffc02325a1
Oleg <olegr1998@ukr.net>
2017-09-21 14:23:12 +0300

    Lab2_1 and Lab 2_2
    some ez job

---
+++ lab2_1.py
@@ -0,0 +1,6 @@
+#! python
+# coding: utf-8
+
+import sys
+
+print('Вітаю!, ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3])
---
+++ lab2_2.py
@@ -0,0 +1,12 @@
+#! python
+# coding: utf-8
+
+import sys
+
+surname = input('Вкажіть своє прізвище \r\n')
+
+name = input('Вкажіть своє ім*я \r\n')
+
+lastname = input('І як вас побатькові\r\n')
+
+print('Вітаю! ' + str(surname) + ' ' + str(name) + ' ' + str(lastname))
