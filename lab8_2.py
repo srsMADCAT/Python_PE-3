7b1b4edf6d1df2169d26c824f3043f9d14bd66fb
Oleg <olegr1998@ukr.net>
2017-10-12 11:12:16 +0300

    lab8_2.py
    just some qwerty

---
+++ lab8_2.py
@@ -0,0 +1,17 @@
+#! python
+# coding: utf-8
+
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+
+alist = [15,65,84,67,12,35,16,97,34,2,15,64]
+selectionSort(alist)
+print(alist)
